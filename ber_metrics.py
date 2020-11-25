93eba7bb4ad5743e3baf1300450e0f1acb9dde3d
Matthew Sun <matthewkol186@gmail.com>
2020-11-24 21:22:06 -0500

    Add bhattacharrya bounds estimate of BER

--- ber_metrics.py
+++ ber_metrics.py
@@ -23,11 +23,12 @@ class BEREstimator:
         """
         self.x = x
         self.y = y
-        self.subgroups = subgroups
+        self.subgroups = subgroups # not currently used
 
     def mahalanobis_bound(self):
         """
-        Calculate the Mahalanobis distance between instances in class 0 and class 1.
+        Calculate the BER upper bound estimate using the Mahalanobis distance between
+        instances in class 0 and class 1.
 
         Equations from Tumer and Ghosh (2003) and also referencing Ryan Holbrook's
         implementations at:
@@ -43,5 +44,27 @@ class BEREstimator:
         sigma_inv = np.linalg.inv(sigma_0 * p_0 + sigma_1 * p_1)
         m_dist = distance.mahalanobis(mu_0, mu_1, sigma_inv) ** 2
         return 2 * p_0 * p_1 / (1 + p_0 * p_1 * m_dist)
+
+    def bhattacharyya_bound(self):
+        """
+        Calculate the BER upper bound estimate using the Bhattacharrya bound
+        between instances in class 0 and class 1.
+
+        Equations from Tumer and Ghosh (2003) and also referencing Ryan Holbrook's
+        implementations at:
+        https://rdrr.io/github/ryanholbrook/bayeserror/src/R/bayeserror.R
+
+        """
+        p_1 = self.y.mean()
+        p_0 = 1 - p_1
+        mu_0 = self.x[self.y == 0, :].mean(axis=0)  # mean vector for class 0 instances
+        mu_1 = self.x[self.y == 1, :].mean(axis=0)  # mean vector for class 1 instances
+        sigma_0 = np.cov(self.x[self.y == 0, :].T)
+        sigma_1 = np.cov(self.x[self.y == 1, :].T)
+        sigma = (sigma_0 + sigma_1) / 2
+        first_term = (1/8) * (mu_1 - mu_0).T @ sigma @ (mu_1 - mu_0)
+        second_term = (1/2) * np.log(np.linalg.det(sigma) / np.sqrt(np.linalg.det(sigma_0) * np.linalg.det(sigma_1)))
+        b_dist = first_term + second_term
+        return np.exp(-b_dist) * np.sqrt(p_0 * p_1) # for now, only interested in upper bound
 
 
