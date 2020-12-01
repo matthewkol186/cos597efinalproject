07d281fc33da567664ca72b7a6421bad0173f40c
Angelina Wang <angelina.wang@berkeley.edu>
2020-12-01 10:37:15 -0500

    singular v nonsingular versions of adult data

--- adult_data.py
+++ adult_data.py
@@ -2,6 +2,15 @@ from sklearn import preprocessing
 import pickle
 import numpy as np
 import pandas as pd
+import argparse
+
+parser = argparse.ArgumentParser(description='diagnosing some errors')
+parser.add_argument('--version', type=int, default=0, help='0 is all columns, 1 is drop first column so matrix is non-singular')
+args = parser.parse_args()
+
+drop_first = False
+if args.version == 1:
+    drop_first = True
 
 # pre-processing and setting up the data
 min_max_scaler = preprocessing.MinMaxScaler()
@@ -40,7 +49,7 @@ for version in ['train', 'test']:
 
         data[['age','fnlwgt','education-num','capital-gain','capital-loss','hours-per-week','label'] ]=data[['age','fnlwgt','education-num','capital-gain','capital-loss','hours-per-week','label']].astype(str).astype(int)
         data_cat = data[['workclass','education','marital-status','occupation','relationship','race','sex','native-country']]
-        data_cat = pd.get_dummies(data_cat)
+        data_cat = pd.get_dummies(data_cat, drop_first=drop_first)
         data = data.drop(['workclass','education','marital-status','occupation','relationship','race','sex','native-country'],axis=1).join(data_cat)
         min_max_scaler.fit(data)
     elif version == 'test':
@@ -49,7 +58,7 @@ data = pd.concat([train_data, test_data])
 
 data[['age','fnlwgt','education-num','capital-gain','capital-loss','hours-per-week','label'] ]=data[['age','fnlwgt','education-num','capital-gain','capital-loss','hours-per-week','label']].astype(str).astype(int)
 data_cat = data[['workclass','education','marital-status','occupation','relationship','race','sex','native-country']]
-data_cat = pd.get_dummies(data_cat)
+data_cat = pd.get_dummies(data_cat, drop_first=drop_first)
 #data = data.drop(['workclass','education','marital-status','occupation','relationship','race','sex','native-country'],axis=1).join(data_cat)
 data = pd.concat([data.drop(['workclass','education','marital-status','occupation','relationship','race','sex','native-country'],axis=1), data_cat], axis=1)
 
@@ -63,4 +72,27 @@ print(train_data.shape, test_data.shape)
 
 X_train, y_train = train_data.drop(['label'], axis=1), train_data['label']
 X_test, y_test = test_data.drop(['label'], axis=1), test_data['label']
-pickle.dump([X_train, y_train, X_test, y_test], open('Data/adult_income/processed_data.pkl', 'wb'))
+
+## To handle matrices that are close to singular, drop one from each one-hot encoding so no linear dependence (jk, drop_first in dummies did it)
+#X_train = X_train.drop(labels=['workclass_Without-pay', 
+#                               'education_Some-college', 
+#                               'marital-status_Widowed', 
+#                               'occupation_Transport-moving', 
+#                               'relationship_Wife', 
+#                               'race_White', 
+#                               'sex_Male', 
+#                               'native-country_Yugoslavia'], axis=1)
+#X_test = X_test.drop(labels=['workclass_Without-pay', 
+#                               'education_Some-college', 
+#                               'marital-status_Widowed', 
+#                               'occupation_Transport-moving', 
+#                               'relationship_Wife', 
+#                               'race_White', 
+#                               'sex_Male', 
+#                               'native-country_Yugoslavia'], axis=1)
+
+if args.version == 0:
+    pickle.dump([X_train, y_train, X_test, y_test], open('Data/adult_income/processed_data.pkl', 'wb'))
+elif args.version == 1:
+    pickle.dump([X_train, y_train, X_test, y_test], open('Data/adult_income/processed_data_nonsingular.pkl', 'wb'))
+
