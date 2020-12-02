f90f5bfbf885a942a544a08d168c331e67a191ca
Angelina Wang <angelina.wang@berkeley.edu>
2020-12-02 16:08:20 -0500

    ensemble code in utils file

---
+++ ensemble_utils.py
@@ -0,0 +1,87 @@
+from sklearn.ensemble import RandomForestClassifier
+import numpy as np
+from sklearn.svm import SVC
+from sklearn.linear_model import LinearRegression, LogisticRegression
+from sklearn.neural_network import MLPClassifier
+from sklearn.ensemble import BaggingClassifier, StackingClassifier
+from sklearn.model_selection import train_test_split
+
+class Ensemble:
+    def __init__(self, version, params):
+        # versions: 0 = mlp bagging, 1 = random forest, 2 = logreg bagging, 3 = svm bagging, 4 = ultimate with 0+1+2+3
+
+        self.version = version
+
+        if 'rf_num' not in params.keys():
+            params['rf_num'] = 100
+        if 'mlp_num' not in params.keys():
+            params['mlp_num'] = 10
+        if 'lr_num' not in params.keys():
+            params['lr_num'] = 10
+        if 'svm_num' not in params.keys():
+            params['svm_num'] = 10
+        self.params = params
+
+        if self.version == 0:
+            self.model = BaggingClassifier(base_estimator=MLPClassifier(max_iter=300), n_estimators=self.params['mlp_num'])
+        elif self.version == 1:
+            self.model = RandomForestClassifier(n_estimators=self.params['rf_num'], class_weight='balanced')
+        if self.version == 2:
+            self.model = BaggingClassifier(base_estimator=LogisticRegression(class_weight='balanced'), n_estimators=self.params['lr_num'])
+        if self.version == 3:
+            self.model = BaggingClassifer(base_estimator=SVC(class_weight='balanced'), n_estimators=self.params['svm_num'])
+        elif self.version == 4:
+            #self.model = StackingClassifier(estimators=[BaggingClassifier(base_estimator=MLPClassifier(max_iter=300), n_estimators=self.params['mlp_num']), 
+            #        RandomForestClassifier(n_estimators=self.params['rf_num'], class_weight='balanced'),
+            #        BaggingClassifier(base_estimator=LogisticRegression(class_weight='balanced'), n_estimators=self.params['lr_num']),
+            #        BaggingClassifier(base_estimator=SVC(class_weight='balanced'), n_estimators=self.params['svm_num'])])
+            self.model = [BaggingClassifier(base_estimator=MLPClassifier(max_iter=300), n_estimators=self.params['mlp_num']), 
+                    RandomForestClassifier(n_estimators=self.params['rf_num'], class_weight='balanced'),
+                    BaggingClassifier(base_estimator=LogisticRegression(class_weight='balanced'), n_estimators=self.params['lr_num']),
+                    BaggingClassifier(base_estimator=SVC(class_weight='balanced'), n_estimators=self.params['svm_num'])]
+            self.combiner = LogisticRegression(class_weight='balanced')
+        else:
+            assert NotImplementedError
+
+    def train(self, X, y):
+        if self.version in [4]:
+            X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.33)
+
+            for i, model in enumerate(self.model):
+                model.fit(X_train, y_train)
+                print("Fitted model type {0} of {1}".format(i, len(self.model)))
+            predictions = []
+            for i, model in enumerate(self.model):
+                predictions.append(model.predict(X_test))
+
+            self.combiner.fit(np.array(predictions).T, y_test)
+        elif self.version in [0, 1, 2, 3]:
+            self.model.fit(X, y)
+
+    # returns [num samples x num classifiers in ensemble]
+    def test(self, X):
+        predictions = []
+        if self.version in [0, 1, 2, 3]:
+            for estimator in self.model.estimators_:
+                predictions.append(estimator.predict(X))
+        elif self.version in [4]:
+            for model in self.model:
+                for estimator in model.estimators_:
+                    predictions.append(estimator.predict(X))
+
+        return np.array(predictions).T
+
+    # returns one prediction per sample of aggregated ensembles
+    def test_agg(self, X):
+        if self.version in [0, 1, 2, 3]:
+            return self.model.predict(X)
+        else:
+            predictions = []
+            for i, model in enumerate(self.model):
+                predictions.append(model.predict(X))
+
+            return self.combiner.predict(np.array(predictions).T)
+
+
+
+
