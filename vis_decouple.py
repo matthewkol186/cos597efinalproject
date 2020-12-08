a1b94ba723cf0922d4c830a7a13ddbc81cec2be5
Angelina Wang <angelina.wang@berkeley.edu>
2020-12-08 15:36:56 -0500

    vis used

--- vis_decouple.py
+++ vis_decouple.py
@@ -2,7 +2,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 import csv
 
-tsv_file = open("ai_decouple.tsv")
+folder = 'cv'
+tsv_file = open("{}_decouple.tsv".format(folder))
 read_tsv = csv.reader(tsv_file, delimiter="\t")
 
 ['', 'Male', '(.1615, .1732)', '(.1340, .1426)', '0.1941', '0.2876', '0.1522']
@@ -56,48 +57,59 @@ for group in ['All', 'Female', 'Male', 'Black', 'White']:
         linestyle='solid'
         if group == 'Male' or group == 'White':
             linestyle='dotted'
-            plt.plot([.4+i*.1, .4+i*.1], mi, c=c, linestyle=linestyle)
-            plt.hlines(mi, (.4+i*.1)- 0.04, (.4+i*.1) + 0.04, colors=c)
+            plt.plot([.1+i*.2, .1+i*.2], mi, c=c, linestyle=linestyle, alpha=1.)
+            plt.hlines(mi, (.1+i*.2)- 0.04, (.1+i*.2) + 0.04, colors=c, alpha=1.)
         elif group == 'Female' or group == 'Black':
-            plt.plot([i*.1, i*.1], mi, c=c, linestyle=linestyle, label=l)
-            plt.hlines(mi, (i*.1)- 0.04, (i*.1) + 0.04, colors=c)
+            plt.plot([i*.2, i*.2], mi, c=c, linestyle=linestyle, label=l)
+            plt.hlines(mi, (i*.2)- 0.04, (i*.2) + 0.04, colors=c)
         else:
-            plt.plot([i*.1, i*.1], mi, c=c, label=l, linestyle=linestyle)
-            plt.hlines(mi, (i*.1)- 0.04, (i*.1) + 0.04, colors=c)
+            plt.plot([i*.2, i*.2], mi, c=c, label=l, linestyle=linestyle)
+            plt.hlines(mi, (i*.2)- 0.04, (i*.2) + 0.04, colors=c)
     for i, plur in enumerate(plurs):
         c = 'C{}'.format(i)
         linestyle='solid'
         add = 0
+        alpha = 1.
         if group == 'Male' or group == 'White':
             linestyle='dotted'
-            add = .4
-        plt.plot([add+1.+i*.1, add+1.+i*.1], plur, c=c, linestyle=linestyle)
-        plt.hlines(plur, (add+1.+i*.1)- 0.04, (add+1.+i*.1) + 0.04, colors=c)
+            add = .1
+            alpha = 1.
+        plt.plot([add+1.+i*.2, add+1.+i*.2], plur, c=c, linestyle=linestyle, alpha=alpha)
+        plt.hlines(plur, (add+1.+i*.2)- 0.04, (add+1.+i*.2) + 0.04, colors=c, alpha=alpha)
     for i, err in enumerate(errs):
         c = 'C{}'.format(i)
         marker = 'o'
         add = 0
         if group == 'Male' or group == 'White':
             marker = 'x'
-            add = .4
-        plt.scatter([add+2.+i*.1], [err], c=c, marker=marker)
+            add = .1
+        plt.scatter([add+2.+i*.2], [err], c=c, marker=marker)
     if group == 'Male' or group == 'White' or group == 'All':
+        val = .3
+        if folder == 'cv':
+            val = .1
         if group == 'Male':
             title = 'Sex'
-            plt.plot([0], [0], c='k', linestyle='solid', label='Female')
-            plt.plot([0], [0], c='k', linestyle='dotted', label='Male')
+            plt.plot([val], [val], c='k', linestyle='solid', label='Female')
+            plt.plot([val], [val], c='k', linestyle='dotted', label='Male', alpha=1.)
+            #plt.plot([.8, .8], [.02, .45], c='k')
+            #plt.plot([1.8, 1.8], [.02, .45], c='k')
+            #plt.plot([2.8, 2.8], [.02, .45], c='k')
         elif group == 'White':
             title = 'Race'
-            plt.plot([0], [0], c='k', linestyle='solid', label='Black')
-            plt.plot([0], [0], c='k', linestyle='dotted', label='White')
-            plt.plot([.8, .8], [0, .45], c='k')
-            plt.plot([1.8, 1.8], [0, .45], c='k')
-            plt.plot([2.8, 2.8], [0, .45], c='k')
+            plt.plot([val], [val], c='k', linestyle='solid', label='Black')
+            plt.plot([val], [val], c='k', linestyle='dotted', label='White', alpha=1.)
+            #plt.plot([.8, .8], [.02, .42], c='k')
+            #plt.plot([1.8, 1.8], [.02, .42], c='k')
+            #plt.plot([2.8, 2.8], [.02, .42], c='k')
         elif group == 'All':
             title = 'All'
+            #plt.plot([.8, .8], [.12, .165], c='k')
+            #plt.plot([1.8, 1.8], [.12, .165], c='k')
+            #plt.plot([2.8, 2.8], [.12, .165], c='k')
         plt.title(title)
         plt.xticks([0.35, 1.35, 2.35], ['MI\nEnsemble', 'Plurality\nEnsemble', 'Classification\nError'])
         plt.legend(bbox_to_anchor=(1.05, 1), loc='upper left')
         plt.tight_layout()
-        plt.savefig('images/{}.png'.format(title), dpi=300)
+        plt.savefig('images/{0}/{1}.png'.format(folder, title), dpi=300)
         plt.close()
